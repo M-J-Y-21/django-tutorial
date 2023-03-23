39715c54a073d764d31a08d23371af1673fb417c
Matan <matanyakobovich@yahoo.com>
2023-03-23 16:52:32 +0100

    First Commit, done upto part 4 before generic views code refactoring.

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Models showns as classes in the database
+# Each model has class variables that represent database fields will be shown as columns
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    # Defines the string representation of the object
+    def __str__(self):
+        return self.question_text
+    
+class Choice(models.Model):
+    # This foreign key says each choice is related to a single question 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
